3db7c364bcb4b57bca63cd92b2445addd634ea52
Danna Michelle Caicedo Valencia <caicedovalenciadannamichelle@gmail.com>
2026-02-20 00:27:18 +0100

    Problema 2 - cliente  REALIZADO

--- problema2/cliente.py
+++ problema2/cliente.py
@@ -6,27 +6,38 @@ Objetivo: Crear un cliente TCP que envíe un mensaje al servidor y reciba la mis
 
 import socket
 
-# TODO: Definir la dirección y puerto del servidor
+#Definir la dirección y puerto del servidor
+HOST = 'localhost'
+PORT = 8000
 
 # Solicitar mensaje al usuario por consola
 message = input("Mensaje: ")
 
-# TODO: Crear un socket TCP/IP
+# Crear un socket TCP/IP
 # AF_INET: socket de familia IPv4
 # SOCK_STREAM: socket de tipo TCP (orientado a conexión)
+cliente = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-# TODO: Conectar el socket al servidor en la dirección y puerto especificados
+#Conectar el socket al servidor en la dirección y puerto especificados
+HOST = 'Localhost'
+PORT = 8000
+cliente.connect((HOST, PORT))
+print("Conexión con éxito")
 
 # Mostrar mensaje que se va a enviar
 print(f"Mensaje '{message}' enviado.")
 
-# TODO: Codificar el mensaje a bytes y enviarlo al servidor
+#Codificar el mensaje a bytes y enviarlo al servidor
 # sendall() asegura que todos los datos sean enviados
+cliente.sendall(b"Mundo")
 
-# TODO: Recibir datos del servidor (hasta 1024 bytes)
+#Recibir datos del servidor (hasta 1024 bytes)
+datos = cliente.recv(1024)
+print(f"Respuesta: {datos}")
 
 # Decodificar e imprimir los datos recibidos
-print("Mensaje recibido: ", data.decode())
+print("Mensaje recibido: ", datos.decode())
 
-# TODO: Cerrar la conexión con el servidor
+#Cerrar la conexión con el servidor
+cliente.close()
 
